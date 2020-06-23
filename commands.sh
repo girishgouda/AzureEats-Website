e2c18209f00a7b47b87e3e0b283cf979448ee8e3
girish <girish@wiprooffice15.onmicrosoft.com>
2020-06-23 13:17:06 +0530

    added the latest files

--- commands.sh
+++ commands.sh
@@ -2,3 +2,6 @@ gitrepo=https://github.com/Microsoft/TailwindTraders
 webappname=challenge1-app-service
 
 az webapp deployment source config --name $webappname --resource-group challenge1 --repo-url $gitrepo --branch master --manual-integration
+
+az ad sp create-for-rbac --name "myApp" --role contributor --scopes /subscriptions/ddb45f69-d4fe-4d69-b32e-262360763207
+/resourceGroups/challenge1/providers/Microsoft.Web/sites/challenge1-app-service
