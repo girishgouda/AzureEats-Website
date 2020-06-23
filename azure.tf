ef55ebc0aed751c5b08756331f347e347762fa34
girish <girish@wiprooffice15.onmicrosoft.com>
2020-06-23 08:12:42 +0530

    challege 1

---
+++ azure.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
+
+
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = "challenge1"
+  location = "southindia"
+}
+
+resource "azurerm_app_service_plan" "appserviceplan" {
+  name                = "challenge1-appserviceplan"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "appservice" {
+  name                = "challenge1-app-service"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
+
+  app_settings = {
+    "WEBSITE_NODE_DEFAULT_VERSION" = "10.15.2"
+    "ApiUrl" = ""
+    "ApiUrlShoppingCart" = ""
+    "MongoConnectionString" = ""
+    "SqlConnectionString" = ""
+    "productImagesUrl" = "https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail"
+    "Personalizer__ApiKey" = ""
+    "Personalizer__Endpoint" = ""
+  }
+
+}
+
+
---
+++ commands.sh
@@ -0,0 +1,2 @@
+
+az webapp deployment source config --name $webappname --resource-group challenge1 --repo-url $gitrepo --branch master --git-token $token
